527c04944c98a8642fc1c3195fbe2f93c741b8e4
Regis Tsafack <regis.tsafack@yahoo.com>
2026-08-13 15:48:22 +0200

    Initial Spring Boot project setup

---
+++ src/test/java/de/thnuernberg/eit/regis/sponsor_tracker/SponsorTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.thnuernberg.eit.regis.sponsor_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SponsorTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
